9250bbb1f24b4893e363fdf6e6491ff44611f427
G99SEMAN <attomix88@gmail.com>
2019-10-26 15:09:52 +0200

    kap8

---
+++ Kap8.py
@@ -0,0 +1,10 @@
+# n = 1
+# while n <1000:
+#     print(n)
+#     n += 1
+a = "ja"
+
+while a == "ja":
+    b=eval(input("Welche Zahl wollen Sie verdoppeln?: "))
+    print(b*2)
+    a = input("möchten sie eine weitere Aufgabe rechnen? (ja/nein)")
