6e9b0335a2cd75c63f7b055f27f1c301bcf06383
G99SEMAN <attomix88@gmail.com>
2019-11-06 20:41:15 +0100

    initiallized

--- objektorientierung.py
+++ objektorientierung.py
@@ -13,8 +13,53 @@ class Auto:
         *Preis (int): Angestrebter Verkaufspreis
         """
 
-        self.Marke = ma
-        self.Modell = mo
-        self.Baujahr = bj
-        self.Preis = pr
-        
+        self.__Marke = ma
+        self.__Modell = mo
+        self.__Baujahr = bj
+        self.__Preis = pr
+
+    def getMarke(self):
+        """
+        Gibt die Marke des Fahrzeugs zurück.
+        """
+        return self.__Marke
+    def getModell(self):
+        """
+        Gibt das Modell des Fahrzeugs aus
+        """
+        return self.__Modell
+
+    def getBaujahr(self):
+        """
+        Gibt das Baujahr des Fahrzeugs aus
+        """
+        return self.__Baujahr
+
+    def getPreis(self):
+        """
+        Gibt den Preis des Fahrzeugs aus
+        """
+        return self.__Preis
+
+    def setPreis(self, preis_neu):
+        """
+        Ändert den Preis des Fahrzeugs
+        """
+        if abs(self.__Preis - preis_neu) < self.__Preis *0.05:
+            self.__Preis = preis_neu
+        else:
+            print("Die Abweichung zum vorherigen Preis ist sehr groß.")
+            bestaetigung = input("Soll %d als neuer Preis festgelegt werden?(ja/nein):"%preis_neu)
+            if bestaetigung == "ja":
+                self.__Preis = preis_neu
+
+
+
+auto1 = Auto("VW", "Golf", 2011, 5000)
+auto2 = Auto("Renault", "Clio", 2013, 6000)
+auto3 = Auto("Posche", "Panamera", 2014, 25000)
+
+wert = eval(input("Geben Sie einen neuen Preis ein: "))
+auto1.setPreis(wert)
+
+print("Neuer Preis:",auto1.getPreis())
