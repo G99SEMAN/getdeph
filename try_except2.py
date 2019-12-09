57091886f91c3231ac50346b6bb8a8273513c13e
G99SEMAN <attomix88@gmail.com>
2019-12-09 16:07:16 +0100

    offds

--- try_except2.py
+++ try_except2.py
@@ -1,18 +1,42 @@
-class OutOfRangeError(Exception):
-    def __init_(self, wert):
-        self.Wert = wert
+"""
+Übungsaufgabe 1
+"""
 
+# try:
+#     z1 = int(input("Bitte geben sie die erste zahl ein:"))
+#     z2 = int(input("Bitte geben sie die zweite zahl ein:"))
+#
+#     print(z1/z2)
+#
+# except ZeroDivisionError:
+#     print("Sie können nicht durch 0 teilen!")
+# except ValueError:
+#     print("Sie müssen eine Zahl eingeben")
+# except:
+#     print("Folgender Fehler ist aufgetreten:", sys.exc_info()[0])               #gibt den Fehler aus, wenn er nicht vorher abgefangen wurde
+#
+# print("Ende!")
+
+
+
+"""
+Übungsaufgabe 2
+"""
+
+class OddError(Exception):
+    self.Wert = wert
 
 try:
-    x = int(input("geben sie eine zahl zwischen 10 und 20 ein:"))
-    if x > 20 or x < 10:
-        raise OutOfRangeError(x)
-    print("Ergebnis:", 4/x)
-except OutOfRangeError as fehler:
-    print("Die zahl", fehler.Wert, "liegt nicht zwichen 10 und 20")
-except ValueError:
-    print("Sie müssen eine ganze zahl eingeben!")
+    wort = input("geben sie ein wort mit einer geraden anzahl an buchstaben ein")
+    if len(wort) %2 == 1:
+        raise OddError(wort)
+    mitte = int(len(wort)/2)
+    teil1 = wort[:mitte]
+    teil2 = wort[mitte:]
+    print("Erste Hälfte:", teil1)
+    print("Zweite Hälfte:", teil2)
 
+except OddError as fehler:
+    print("Das Wort", wort, "hat eine ungerade Anzahl an buchstaben.")
 
-print("auf wiedersehen")
-print("hello")
+print("Auf wiedersehen!")
