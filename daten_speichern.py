57e62794b0e5b4fda5d934c603e6550463ee46aa
G99SEMAN <attomix88@gmail.com>
2019-12-09 16:46:27 +0100

    grundlagen

--- daten_speichern.py
+++ daten_speichern.py
@@ -5,16 +5,19 @@ w       Datei wird nur zum Schreiben geöffnet.
 a       Das Programm hängt die neuen Eingaben an eine bestehende Datei an.
 r+      Die Datei wird zum Lesen und zum Schreiben geöffnet.
 """
-
+f = open("dateien/sortiment.txt", "r")
+inhalt = f.readlines()
+print (len(inhalt))
+print(inhalt)
 
 f = open("dateien/sortiment.txt", "a")
 
-marke = input("Geben Sie eine Marke ein: ")
-modell = input("Geben Sie ein Modell ein: ")
-baujahr = input("Geben Sie ein Baujahr ein: ")
-preis = input("Geben Sie einen Preis ein: ")
-
-f.write(marke+"\n")
-f.write(modell+"\n")
-f.write(baujahr+"\n")
-f.write(preis+"\n")
+# marke = input("Geben Sie eine Marke ein: ")
+# modell = input("Geben Sie ein Modell ein: ")
+# baujahr = input("Geben Sie ein Baujahr ein: ")
+# preis = input("Geben Sie einen Preis ein: ")
+#
+# f.write(marke+"\n")
+# f.write(modell+"\n")
+# f.write(baujahr+"\n")
+# f.write(preis+"\n")
