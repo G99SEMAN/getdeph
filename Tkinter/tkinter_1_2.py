ad89b98025b8ef170267df44814715750cde4506
G99SEMAN <attomix88@gmail.com>
2019-12-16 12:57:42 +0100

    grid to place

--- Tkinter/tkinter_1_2.py
+++ Tkinter/tkinter_1_2.py
@@ -12,13 +12,13 @@ rahmen = Frame(fenster, relief="ridge", borderwidth=3)
 rahmen.pack(fill="both", expand=1)
 
 button = Button(rahmen,width=10, text="OK", command=fenster.destroy)
-button.grid(row=2, column=2, pady=10)
+button.place(x=40, y=60)
 button2 = MyButton(rahmen, width=10, text="Action!")
 button2["command"] = button2.aktion
 button2.grid(row=2, column=1)
 
 label = Label(rahmen, text="Wilkommenzum Python Kurs!")
-label.grid(row=1, column=1, columnspan=2, padx=50, pady=15)
+label.place(x=600, y=20)
 
 
 fenster.mainloop()
